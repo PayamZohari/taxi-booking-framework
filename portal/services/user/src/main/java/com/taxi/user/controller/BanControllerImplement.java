505320b465ee0286b90e2ec7308942bab32b1200
“HoseinVatani” <Hosein.vatani20@gmail.com>
2024-07-05 19:46:46 +0330

    fix url api/admin

--- portal/services/user/src/main/java/com/taxi/user/controller/BanControllerImplement.java
+++ portal/services/user/src/main/java/com/taxi/user/controller/BanControllerImplement.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/api/admin/banuser")
+@RequestMapping("/api/admin")
 @RestController
 public class BanControllerImplement extends AbstractBanController<ExtendedBanDTO> {
 
