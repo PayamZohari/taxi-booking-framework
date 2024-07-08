737843e6ca3a46bdf0dfdea2ed50ea5f0d66f503
“HoseinVatani” <Hosein.vatani20@gmail.com>
2024-07-08 23:45:54 +0330

    create model

---
+++ portal/services/user/src/main/java/com/taxi/user/model/AnalyticReport.java
@@ -0,0 +1,24 @@
+package com.taxi.user.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class AnalyticReport {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String totalPassengersPerDay;
+    private String totalTripsPerDay;
+    private String totalActiveDriversPerDay;
+    private String totalProfitPerDay;
+
+
+}
